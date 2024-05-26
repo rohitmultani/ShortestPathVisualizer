09abb195d07a5bf1833414586af22916f0af56e2
Rohit Multani <rohitmultani@153.com>
2024-05-26 21:35:50 +0530

    deployement changes

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import NavBar from './components/NavBar';
 import Grid from './components/Grid';
+import { Analytics } from "@vercel/analytics/react"
 import { VisualizerProvider } from './context/VisualizerContext';
 import Intro from './components/Intro';
 
 const App = () => {
   return (
     <VisualizerProvider>
+      <Analytics/>
       <Intro />
       <NavBar />
       <div className="p-4">
